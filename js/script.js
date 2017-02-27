07ffe8a71c3d584dba28226148dd2e5d3f293b47
Sally <sally.luong@gmail.com>
2017-02-26 23:26:07 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('slow');
+
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('slow');
+
+    });
+});
