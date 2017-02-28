efe5a8a515a0103957cb0b2c4808991ce44fbb98
Sally <sally.luong@gmail.com>
2017-02-27 20:53:35 -0500

    Toggles hide on click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+//adapted from code by david@uniondesign.ca
+
 $(document).ready(function($) {
     $('#accordion').find('.accordion-toggle').click(function(){
 
-      //Expand or collapse this panel
-      $(this).next().slideToggle('slow');
+      	//Expand or collapse this panel
+    	$(this).next().slideToggle('slow');
 
-      //Hide the other panels
-      $(".accordion-content").not($(this).next()).slideUp('slow');
+      	//Hide the other panels
+      	$(".accordion-content").not($(this).next()).slideUp('slow');
+
+    	//Hide the toggle for current panel
+     	$(this).slideUp('slow');
+
+     	//Re-opens closed toggles
+    	$(".accordion-toggle").not($(this)).slideDown('slow');
 
     });
 });
