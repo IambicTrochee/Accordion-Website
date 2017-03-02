eb8e8599d3c40b138cc8e4d403298e3bb9fd78db
Sally <sally.luong@gmail.com>
2017-03-02 00:25:01 -0500

    Form styling

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-//adapted from code by david@uniondesign.ca
-//http://uniondesign.ca/simple-accordion-without-jquery-ui/
-
 $(document).ready(function($) {
-    $('#accordion').find('.accordion-toggle').click(function(){
+	//Adapted from code by david@uniondesign.ca
+	//http://uniondesign.ca/simple-accordion-without-jquery-ui/
+	//Enables accordion
+	$('#accordion').find('.accordion-toggle').click(function(){
 
-      	//Expand or collapse this panel
-    	$(this).next().slideToggle('slow');
+    //Expand selected panel
+    $(this).next().slideToggle('slow');
 
-      	//Hide the other panels
-      	$(".accordion-content").not($(this).next()).slideUp('slow');
+    //Hide the other panels
+    $(".accordion-content").not($(this).next()).slideUp('fast');
 
-    	//Hide the toggle for current panel
-     	$(this).slideUp('slow');
+    //Hide the toggle for current panel
+    $(this).slideUp('fast');
 
-     	//Re-opens closed toggles
-    	$(".accordion-toggle").not($(this)).slideDown('slow');
+    //Re-opens closed toggles
+    $(".accordion-toggle").not($(this)).slideDown('fast');
+
+    //Adapted from code by Davod Darnes on EnvatoTuts
+    //https://webdesign.tutsplus.com/tutorials/quick-tip-add-a-formspree-form-to-your-static-sites--cms-23870
+    //Enables Formspree and avoids posting email address in plaintext
+    var contact =  document.getElementById('contactform');
+    contact.setAttribute('action', 'https://formspree.io/' + 'sal' + 'lyluo' + 'ng.pu' + 'blic' + '@g' + 'mail' + '.' + 'com');
 
     });
 });
